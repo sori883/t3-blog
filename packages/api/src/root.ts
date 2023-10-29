495e51efb25f0c8087aff61038fb8096b0a52a5f
sorinaji <37400002+sori883@users.noreply.github.com>
2023-10-29 21:25:23 +0900

    create:api

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,10 +1,14 @@
 import { authRouter } from "./router/auth";
+import { categoryRouter } from "./router/category";
 import { postRouter } from "./router/post";
+import { tagRouter } from "./router/tag";
 import { createTRPCRouter } from "./trpc";
 
 export const appRouter = createTRPCRouter({
   auth: authRouter,
   post: postRouter,
+  category: categoryRouter,
+  tag: tagRouter,
 });
 
 // export type definition of API
