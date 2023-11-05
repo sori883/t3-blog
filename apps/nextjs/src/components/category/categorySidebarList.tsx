9ee4c5da00fc6145ce1368bbb9647c00aa4b80cb
sorinaji <37400002+sori883@users.noreply.github.com>
2023-11-05 13:08:46 +0900

    sidebar

---
+++ apps/nextjs/src/components/category/categorySidebarList.tsx
@@ -0,0 +1,22 @@
+import { api } from "~/utils/server";
+import { PostCard } from "~/components/post"
+
+export async function CategorySidebarList() {
+  const posts = await api.post.index.query({ limit: 10, offset: 0 });
+
+  if (posts.length === 0) {
+    return (
+      <div className="relative flex w-full flex-col gap-4">
+      <div className="absolute inset-0 flex flex-col items-center justify-center bg-black/10">
+        <p className="text-2xl font-bold text-white">No post yet</p>
+      </div>
+    </div>
+    );
+  }
+
+  return (
+    <div className="grid grid-cols-2 gap-4">
+      {posts.map((p) => (<PostCard key={p.id} post={p} />))}
+    </div>
+  );
+}
--- apps/nextjs/src/components/layouts/baseLayout.tsx
+++ apps/nextjs/src/components/layouts/baseLayout.tsx
@@ -1,11 +1,20 @@
 import { Header, Footer } from "~/components/layouts";
+import { Link } from "~/components/elements/link"
 
 export const BaseLayout = ({ children }: { children: React.ReactNode }) => (
   <div>
     <Header />
-    <div className="flex w-full">
-      <div className="container mx-auto flex min-h-screen max-w-full flex-1 flex-col">
+    <div className="my-12 container mx-auto min-h-screen grid grid-cols-1 md:grid-cols-4 gap-4">
+      <div className="colspan-1 md:col-span-3">
         {children}
+      </div>
+      <div className="colspan-1 bg-base-200">
+      <ul className="ui_menu bg-base-200 ui_rounded-box w-full">
+        <li className="ui_menu-title">Title</li>
+        <li><Link>Item 1</Link></li>
+        <li><Link>Item 2</Link></li>
+        <li><Link>Item 3</Link></li>
+      </ul>
       </div>
     </div>
     <Footer />
