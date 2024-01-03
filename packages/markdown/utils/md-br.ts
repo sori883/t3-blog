1fe586f889101c65975a39f031e24d98d0b09a25
sorinaji <37400002+sori883@users.noreply.github.com>
2024-01-03 15:02:34 +0900

    #2 markdown移行

---
+++ packages/markdown/utils/md-br.ts
@@ -0,0 +1,98 @@
+/**
+ * forked from https://github.com/zenn-dev/zenn-editor
+ */
+import type MarkdownIt from 'markdown-it';
+
+/**
+ * Return case-sensitive matched br tag
+ * @param {any} state MarkdownIt state
+ * @param {number} start start position at br tag
+ * @returns {string | null} br tag (<br> or <br/> or <br />)
+ */
+// eslint-disable-next-line
+function matchBR(state: any, start: number): string | null {
+  // eslint-disable-next-line
+  const match = state.src.slice(start, start + 6).match(/^<br\s?\/?>/);
+  if (match) {
+    // eslint-disable-next-line
+    return match[0];
+  }
+  return null;
+}
+
+export function mdBr(md: MarkdownIt): void {
+  // Tokenize
+  md.inline.ruler.before(
+    'emphasis',
+    'br',
+    // eslint-disable-next-line
+    function tokenize(state: any, silent): boolean {
+          // eslint-disable-next-line
+      const max = state.posMax;
+          // eslint-disable-next-line
+      const start = state.pos;
+
+      // don't run any pairs in validation mode
+      if (silent) {
+        return false;
+      }
+
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      const br = matchBR(state, start);
+      if (br === null || start + br.length > max) {
+        return false;
+      }
+      // eslint-disable-next-line
+      state.scanDelims(state.pos, true);
+      // eslint-disable-next-line
+      const token = state.push('text', '', 0);
+      // eslint-disable-next-line
+      token.content = '<br>';
+      // eslint-disable-next-line
+      state.delimiters.push({
+        // eslint-disable-next-line
+        marker: token.content,
+        jump: 0,
+        // eslint-disable-next-line
+        token: state.tokens.length - 1,
+        // eslint-disable-next-line
+        level: state.level,
+        end: -1,
+        open: true,
+        close: true,
+      });
+
+      // length is: <br> -> 4, <br/> -> 5, <br /> -> 6
+      // eslint-disable-next-line
+      state.pos += br.length;
+
+      return true;
+    }
+  );
+
+  // Walk through delimiter list and replace text tokens with tags
+  md.inline.ruler2.before('emphasis', 'br', function postProcess(state) {
+    let i;
+    let delim;
+    let token;
+    const delimiters = state.delimiters;
+    const max = state.delimiters.length;
+
+    for (i = 0; i < max; i++) {
+      delim = delimiters[i]!;
+      
+      const marker = delim.marker as number | '<br>';
+
+      if (marker === '<br>') {
+        token = state.tokens[delim.token]!;
+        token.type = 'br_openclose';
+        token.tag = 'br';
+        token.nesting = 1;
+        token.markup = '<br>';
+        token.content = '';
+      }
+    }
+
+    return true;
+  });
+}
---
+++ packages/markdown/utils/md-linkify-to-card.ts
@@ -0,0 +1,119 @@
+/**
+ * forked from https://github.com/zenn-dev/zenn-editor
+ */
+import type MarkdownIt from 'markdown-it';
+import Token from 'markdown-it/lib/token';
+import type { MarkdownOptions } from '../types';
+import { generateLinkifyEmbedHTML } from './embed-helper';
+
+function convertAutolinkToEmbed(
+  inlineChildTokens: Token[],
+  options: MarkdownOptions
+): Token[] {
+  const newTokens: Token[] = [];
+
+  inlineChildTokens.forEach((token, i) => {
+    // 埋め込み対象となるlink_open かつ linkify以外はそのまま出力結果に含める
+    if (!(token.type === 'link_open' && token.markup === 'linkify')) {
+      newTokens.push(token); // 変換は行わずに出力結果に含める
+      return;
+    }
+
+    const linkOpenToken = token;
+
+    // tokenがlinkifyの場合は必要に応じて、カードを生成
+    const url = linkOpenToken.attrGet('href');
+    if (!url) {
+      newTokens.push(token); // 変換は行わずに出力結果に含める
+      return;
+    }
+
+    const isStartOfLine = i === 0;
+    const isEndOfLine = i + 2 === inlineChildTokens.length - 1; // i + 2 = link_closeのこと => link_closeが最後のtokenか
+
+    const prevToken = isStartOfLine ? null : inlineChildTokens[i - 1]; // e.g. [✋, link_open, text, link_close]
+    const nextToken = isEndOfLine ? null : inlineChildTokens[i + 3]; // e.g. [text, link_open, text, link_close, ✋]
+
+    const isPrevBr = prevToken?.tag === 'br';
+    const isNextBr = nextToken?.tag === 'br';
+
+    // 以下の2つをどちらも満たした場合にリンク化
+    // 1. パラグラフ先頭 もしくは リンクの前が br
+    // 2. パラグラフの末尾 もしくは リンクの後が br
+
+    const shouldConvertToCard =
+      (isStartOfLine || isPrevBr) && (isEndOfLine || isNextBr);
+
+    if (!shouldConvertToCard) {
+      newTokens.push(token); // 変換は行わずに出力結果に含める
+      return;
+    }
+
+    // 埋め込み用のHTMLを生成
+    const embedToken = new Token('html_inline', '', 0);
+
+    // 埋め込み要素のHTML生成
+    embedToken.content = generateLinkifyEmbedHTML(url, options);
+
+    // a要素自体はカードにより不要になるため非表示に
+    linkOpenToken.attrJoin('style', 'display: none');
+
+    // カードとリンクのトークンを出力結果のtokenに追加
+    newTokens.push(embedToken, linkOpenToken);
+
+    // 前後のbrタグはスペースを広げすぎてしまうため非表示にしておく
+    if (nextToken && isNextBr) {
+      nextToken.type = 'html_inline';
+      nextToken.content = '<br style="display: none">\n';
+    }
+    if (prevToken && isPrevBr) {
+      prevToken.type = 'html_inline';
+      prevToken.content = '<br style="display: none">\n';
+    }
+  });
+  return newTokens;
+}
+
+export function mdLinkifyToCard(md: MarkdownIt, options?: MarkdownOptions) {
+  md.core.ruler.after('replacements', 'link-to-card', function ({ tokens }) {
+    // 埋め込みを許可するネストレベル
+    let allowLevel = 0;
+
+    // 本文内のすべてのtokenをチェック
+    tokens.forEach((token, i) => {
+      if (token.type === 'container_details_open') {
+        allowLevel++;
+        return;
+      }
+      if (token.type === 'container_details_close' && allowLevel > 0) {
+        allowLevel--;
+        return;
+      }
+
+      // autolinkはinline内のchildrenにのみ存在
+      if (token.type !== 'inline') return;
+
+      // childrenが存在しない場合は変換しない
+      const children = token.children;
+      if (!children) return;
+
+      // childrenにautolinkが存在する場合のみ変換
+      const hasAnyAutolink = children?.some(
+        (child) => child.markup === 'linkify'
+      );
+      if (!hasAnyAutolink) return;
+
+      // 親がコンテンツ直下のp要素の場合のみ変換
+      const parentToken = tokens[i - 1];
+      const isParentRootParagraph =
+        parentToken &&
+        parentToken.type === 'paragraph_open' &&
+        parentToken.level === allowLevel;
+      if (!isParentRootParagraph) return;
+
+      token.children = convertAutolinkToEmbed(children, options ?? {});
+    });
+
+    return true;
+  });
+}
