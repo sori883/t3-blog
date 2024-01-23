e011fd59dfae21b85021c6884b90bb790bf4a873
sorinaji <37400002+sori883@users.noreply.github.com>
2024-01-23 21:28:36 +0900

    1200px以上の画像を1200pxにリサイズするように変更

--- apps/contentapp/src/utils/convertImage.ts
+++ apps/contentapp/src/utils/convertImage.ts
@@ -3,9 +3,18 @@ import sharp from "sharp";
 // eslint-disable-next-line
 export async function convertImage(image: any) {
   // eslint-disable-next-line
-  const buffer = Buffer.from(image)
+  const buffer = Buffer.from(image);
 
-  return await sharp(buffer)
-    .toFormat("webp", { quality: 80 })
-    .toBuffer()
+  const imageSharp = sharp(buffer);
+  const metadata = await imageSharp.metadata();
+
+  // 横幅が1200px以上の場合、アスペクト比を保ったまま横幅1200pxにリサイズする
+  if (metadata.width && metadata.width > 1200) {
+    imageSharp.resize(1200, null, {
+      withoutEnlargement: true, // 元のサイズより大きくならないようにする
+    });
+  }
+
+  // webpに変換して返す
+  return await imageSharp.toFormat("webp", { quality: 75 }).toBuffer();
 }
